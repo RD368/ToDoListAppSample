5fe22c33d3d0e4e03d2299f78c892e630d63ced3
unknown <ram33694@gmail.com>
2024-08-28 13:46:24 +0530

    First commit of todoapp

---
+++ ToDoListApp/Models/ToDoList.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListApp.Models
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Content")]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(2500)]
+        public string Note { get; set; }
+
+        [Required]
+        [DisplayName("Addition Time")]
+        public DateTime AdditionTime { get; set; }
+
+        [DisplayName("Expected Time")]
+        public string ExpectedTime { get; set; }
+    }
+}
